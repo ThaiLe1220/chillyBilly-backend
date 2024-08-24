fe7c73ad3df1f36f0b9c0728af81b7be14aae50f
ThaiLe1220 <lehongthai2000@gmail.com>
2024-08-24 18:34:11 +0700

    update tokenizer_vi and code to download vi_autoregressive

--- tts_api/tortoise/models/utils.py
+++ tts_api/tortoise/models/utils.py
@@ -1,5 +1,8 @@
 import os
 import re
+import requests
+import progressbar
+from dotenv import load_dotenv
 
 try:
     import gdown
@@ -12,6 +15,8 @@ from urllib import request
 
 import progressbar
 
+load_dotenv()
+
 D_STEM = "https://drive.google.com/uc?id="
 
 DEFAULT_MODELS_DIR = os.path.join(
@@ -20,6 +25,7 @@ DEFAULT_MODELS_DIR = os.path.join(
 MODELS_DIR = os.environ.get("TORTOISE_MODELS_DIR", DEFAULT_MODELS_DIR)
 MODELS = {
     "autoregressive.pth": "https://huggingface.co/jbetker/tortoise-tts-v2/resolve/main/.models/autoregressive.pth",
+    "autoregressive_vi.pth": "https://huggingface.co/Eugenememe/vietnamese_langugage_gpt/resolve/main/autoregressive_vi.pth?download=true",
     "classifier.pth": "https://huggingface.co/jbetker/tortoise-tts-v2/resolve/main/.models/classifier.pth",
     "clvp2.pth": "https://huggingface.co/jbetker/tortoise-tts-v2/resolve/main/.models/clvp2.pth",
     "cvvp.pth": "https://huggingface.co/jbetker/tortoise-tts-v2/resolve/main/.models/cvvp.pth",
@@ -34,6 +40,35 @@ MODELS = {
 
 
 pbar = None
+HF_TOKEN = os.environ.get("HF_TOKEN")
+
+
+def show_progress(block_num, block_size, total_size):
+    global pbar
+    if pbar is None:
+        pbar = progressbar.ProgressBar(maxval=total_size)
+        pbar.start()
+
+    downloaded = block_num * block_size
+    if downloaded < total_size:
+        pbar.update(downloaded)
+    else:
+        pbar.finish()
+        pbar = None
+
+
+def download_file(url, filename):
+    headers = {"Authorization": f"Bearer {HF_TOKEN}"} if "huggingface.co" in url else {}
+    with requests.get(url, stream=True, headers=headers, timeout=30) as r:
+        r.raise_for_status()
+        total_size = int(r.headers.get("content-length", 0))
+        block_size = 8192
+
+        with open(filename, "wb") as f:
+            for chunk in r.iter_content(chunk_size=block_size):
+                if chunk:
+                    f.write(chunk)
+                    show_progress(1, len(chunk), total_size)
 
 
 def download_models(specific_models=None):
@@ -41,19 +76,6 @@ def download_models(specific_models=None):
     Call to download all the models that Tortoise uses.
     """
     os.makedirs(MODELS_DIR, exist_ok=True)
-
-    def show_progress(block_num, block_size, total_size):
-        global pbar
-        if pbar is None:
-            pbar = progressbar.ProgressBar(maxval=total_size)
-            pbar.start()
-
-        downloaded = block_num * block_size
-        if downloaded < total_size:
-            pbar.update(downloaded)
-        else:
-            pbar.finish()
-            pbar = None
 
     for model_name, url in MODELS.items():
         if specific_models is not None and model_name not in specific_models:
@@ -64,8 +86,10 @@ def download_models(specific_models=None):
         print(f"Downloading {model_name} from {url}...")
         if D_STEM in url:
             gdown.download(url, model_path, quiet=False)
+        elif "huggingface.co" in url:
+            download_file(url, model_path)
         else:
-            request.urlretrieve(url, model_path, show_progress)
+            request.urlretrieve(url, model_path)
         print("Done.")
 
 
@@ -78,8 +102,7 @@ def get_model_path(model_name, models_dir=MODELS_DIR):
         model_path = os.path.join(models_dir, model_name)
     else:
         # Check for custom language models
-        base_model_name = re.sub(r"(_jp|_vi)\.pth$", ".pth", model_name)
-        if base_model_name in MODELS:
+        if model_name in MODELS:
             model_path = os.path.join(models_dir, model_name)
         else:
             raise ValueError(f"Model {model_name} not found in available models.")
